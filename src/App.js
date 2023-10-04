332aae5a7fb7b7a3127f8d430b7bd35ac2c5618b
hollarteju <sirteju2022@gmail.com>
2023-10-05 00:33:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <div className='app-container'>
       <LocationContextProvider>
       <UserDetailsProvider>
-        <Router basename='/alert'>
+        <Router basename='/Alert'>
           <Navbar/>
             <Routes>
             
