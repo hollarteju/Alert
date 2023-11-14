ed6ab9633ecea8fae9b17a45bb757d5984e58499
hollarteju <sirteju2022@gmail.com>
2023-11-14 15:58:59 +0100

    pythonanywhere url replacement

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -73,7 +73,7 @@ const SignUp=()=>{
        
         try{
             
-            const response = await fetch("http://127.0.0.1:8000/register",{
+            const response = await fetch("Hollarteju.pythonanywhere.com/register",{
             method:"POST",
             headers:{"Content-Type": "application/json"},
             body: JSON.stringify(registeration_data)
--- src/user_details.js
+++ src/user_details.js
@@ -63,7 +63,7 @@ const login =async(e)=>{
     const Error = {...Access_login};
     try{
         
-        const response = await fetch("http://127.0.0.1:8000//token",{
+        const response = await fetch("Hollarteju.pythonanywhere.com/token",{
         method:"POST",
         headers:{"Content-Type": "application/json"},
         body: JSON.stringify(login_data)
@@ -146,25 +146,7 @@ const avatar_upload=(event)=>{
             save_button.textContent = "Upload Declined............"
            
         }
-        // formData.delete("file");
-        // formData.append("file", Avatar)
-     
-        // const avatar_response = await fetch("https://api.cloudinary.com/v1_1/dxsvadizj/image/upload",{
-        //     method: "POST",
-        //     body:formData,
-        // })
-
         
-        // if(avatar_response.ok){
-        // const data = await avatar_response.json()
-        // setAvatar(data.secure_url)
-        // save_button.textContent = "succesful"
-        // // setSaveImage(true)
-        // }
-        // else{
-        //     console.log("bad")
-           
-        // }
         }catch(error){
             
             save_button.textContent = "Upload Declined............"
@@ -228,7 +210,7 @@ const avatar_upload=(event)=>{
         } 
         try{
             
-            const response = await fetch("http://127.0.0.1:8000//save_images",{
+            const response = await fetch("Hollarteju.pythonanywhere.com/save_images",{
             method:"POST",
             headers:{"Content-Type": "application/json"},
             body: JSON.stringify({"image":Image, "username":user_username, "user":change_username, "avatar":Avatar, "bio":change_bio})
@@ -260,7 +242,7 @@ const avatar_upload=(event)=>{
     const profile_picture =async()=>{
             let username = localStorage.getItem("user")?localStorage.getItem("user"):Token.username
           try{
-              const response = await fetch("http://127.0.0.1:8000//profile_image_response",{
+              const response = await fetch("Hollarteju.pythonanywhere.com/profile_image_response",{
                   method:"POST",
                   headers:{"Content-Type": "application/json"},
                   body: JSON.stringify({"username":username})
@@ -342,7 +324,7 @@ const timeline_api =async(e)=>{
         
    
     try{
-        const response = await fetch("http://127.0.0.1:8000//timeline_update",{
+        const response = await fetch("Hollarteju.pythonanywhere.com/timeline_update",{
             method:"POST",
             headers:{"Content-Type": "application/json"},
             body: JSON.stringify({"username":user_username,
@@ -365,7 +347,7 @@ const timeline_api =async(e)=>{
 
 const timeline_api_response =async()=>{
     try{
-        const response = await fetch("http://127.0.0.1:8000//timeline_res",{
+        const response = await fetch("Hollarteju.pythonanywhere.com/timeline_res",{
             method:"POST",
             headers:{"Content-Type": "application/json"}
             // body: JSON.stringify({"username":user_username})
@@ -426,7 +408,7 @@ const timeline_messages = async(id)=>{
     }
     
     try{
-        const response = await fetch("http://127.0.0.1:8000//timeline_messages",{
+        const response = await fetch("Hollarteju.pythonanywhere.com/timeline_messages",{
             method:"POST",
             headers:{"Content-Type": "application/json"},
             body: JSON.stringify({"username":user_username,"id":id ,"message":message, "media":message_media})
@@ -447,7 +429,7 @@ const timeline_messages_res = async(id)=>{
     setMessages_toggle(id)
     setPost_res([])
     try{
-        const response = await fetch("http://127.0.0.1:8000/timeline_message_response",{
+        const response = await fetch("Hollarteju.pythonanywhere.com/timeline_message_response",{
             method:"POST",
             headers:{"Content-Type": "application/json"},
             body: JSON.stringify({"id":id })
@@ -469,7 +451,7 @@ const timeline_messages_res = async(id)=>{
 const timeline_reaction = async(id,reaction)=>{
    
     try{
-        const response = await fetch("http://127.0.0.1:8000//reaction_update",{
+        const response = await fetch("Hollarteju.pythonanywhere.com/reaction_update",{
             method:"POST",
             headers:{"Content-Type": "application/json"},
             body: JSON.stringify({"username":user_username,"id":id ,"reaction":reaction})
@@ -490,7 +472,7 @@ const timeline_reaction = async(id,reaction)=>{
 
 const user_reaction = async()=>{
     try{
-        let response = await fetch("http://127.0.0.1:8000/user_reaction",{
+        let response = await fetch("Hollarteju.pythonanywhere.com/user_reaction",{
             method:"POST",
             headers:{"Content-Type": "application/json"},
         })
